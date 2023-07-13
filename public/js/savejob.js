2d2ed214a3d19934840ee8ee6dd026a081a1471e
vlada_caban <vladaonly@gmail.com>
2023-07-13 19:48:10 -0400

    added form to save job

---
+++ public/js/savejob.js
@@ -0,0 +1,13 @@
+
+
+// instance.getSelectedValues();
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   let elems = document.querySelectorAll("select");
+//   let instances = M.FormSelect.init(elems, options);
+  
+// });
+
+//   $(document).ready(function () {
+//     $("select").formSelect();
+//   });
