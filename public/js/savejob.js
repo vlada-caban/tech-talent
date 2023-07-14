2d18e1fd0f05615ceb5b27daf0ec95ee8d9f23e7
vlada_caban <vladaonly@gmail.com>
2023-07-13 20:49:06 -0400

    added javascript to save job

--- public/js/savejob.js
+++ public/js/savejob.js
@@ -1,13 +1,43 @@
 
+const saveJobFormHandler = async (e) => {
+  e.preventDefault();
 
-// instance.getSelectedValues();
+  console.log("Save Job was pressed!");
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   let elems = document.querySelectorAll("select");
-//   let instances = M.FormSelect.init(elems, options);
-  
-// });
+  const note_title = document.querySelector("#note_title").value;
+  const note_body = document.querySelector("#note_body").value;
+  const status = document.getElementById("status").value;
 
-//   $(document).ready(function () {
-//     $("select").formSelect();
-//   });
+  const urlArr = window.location.toString().split("/");
+  const job_id = urlArr[urlArr.length - 1];
+
+  console.log(status);
+  console.log(note_title);
+  console.log(note_body);
+  console.log(job_id);
+
+    // const response = await fetch("/post", {
+    //   method: "POST",
+    //   body: JSON.stringify({ post_title, post_body }),
+    //   headers: { "Content-Type": "application/json" },
+    // });
+
+    // if (response.ok) {
+    //   document.location.replace(`/dashboard`);
+    // } else {
+    //   alert("Failed to save job.");
+    // }
+};
+
+document
+  .querySelector("#save-job-btn")
+  .addEventListener("click", saveJobFormHandler);
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  let elems = document.querySelectorAll("select");
+  let instances = M.FormSelect.init(elems);
+});
+
+
+
